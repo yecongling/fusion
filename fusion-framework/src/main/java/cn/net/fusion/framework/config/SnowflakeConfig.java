76e9e2ac75d9ff5ede9bcb4f468fb7b427e2b725
ycl <499475142@qq.com>
2024-11-04 11:45:10 +0800

    feat(功能新增): 传输解密、雪花算法、响应封装
    1、添加后端接受数据进行数据解密功能
    2、添加雪花ID算法，用于生成唯一ID使用
    3、添加响应拦截器，并封装响应结果，确保返回类型一致
    4、添加密码加密工具类

---
+++ fusion-framework/src/main/java/cn/net/fusion/framework/config/SnowflakeConfig.java
@@ -0,0 +1,22 @@
+package cn.net.fusion.framework.config;
+
+import cn.net.fusion.framework.utils.SnowFlakeGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName SnowflakeConfig
+ * @Description 雪花算法的bean生成
+ * @Author ycl
+ * @Date 2024/11/4 10:14
+ * @Version 1.0
+ */
+@Configuration
+public class SnowflakeConfig {
+    @Bean
+    public SnowFlakeGenerator snowFlakeGenerator() {
+        long workerId = 1L;
+        long datacenterId = 1L;
+        return new SnowFlakeGenerator(workerId, datacenterId);
+    }
+}
---
+++ fusion-framework/src/main/java/cn/net/fusion/framework/interceptor/ResponseInterceptor.java
@@ -0,0 +1,36 @@
+package cn.net.fusion.framework.interceptor;
+
+import cn.net.fusion.framework.core.Response;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * @ClassName ResponseInterceptor
+ * @Description 统一返回数据格式
+ * @Author ycl
+ * @Date 2024/11/4 10:19
+ * @Version 1.0
+ */
+@ControllerAdvice
+public class ResponseInterceptor implements ResponseBodyAdvice<Object> {
+
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        if (body instanceof Response) {
+            return body;
+        }
+        // 保证返回的数据是统一格式
+        return Response.success(body);
+    }
+}
