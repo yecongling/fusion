94cfe0d3de077a837ba5b702e545a4f7d11fe56b
ycl <499475142@qq.com>
2024-11-04 18:07:50 +0800

    feat(功能新增): 常量定义
    1、添加常量定义类

---
+++ fusion-framework/src/main/java/cn/net/fusion/framework/constant/CommonConstant.java
@@ -0,0 +1,11 @@
+package cn.net.fusion.framework.constant;
+
+/**
+ * @ClassName CommonConstant
+ * @Description 通用常量定义
+ * @Author ycl
+ * @Date 2024/11/4 17:59
+ * @Version 1.0
+ */
+public interface CommonConstant {
+}
