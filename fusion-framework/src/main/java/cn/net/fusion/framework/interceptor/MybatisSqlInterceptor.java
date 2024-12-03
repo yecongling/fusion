474ace11d0b9b615dad5f5b0a2f3e06bc1c56a73
ycl <499475142@qq.com>
2024-12-03 10:02:43 +0800

    feat(springboot): 升级springboot
    升级springboot->3.4.0

--- fusion-framework/src/main/java/cn/net/fusion/framework/interceptor/MybatisSqlInterceptor.java
+++ fusion-framework/src/main/java/cn/net/fusion/framework/interceptor/MybatisSqlInterceptor.java
@@ -14,6 +14,7 @@ import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.type.TypeHandlerRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
@@ -40,6 +41,9 @@ public class MybatisSqlInterceptor implements Interceptor {
     // 日志记录工具
     private static final Logger logger = LoggerFactory.getLogger(MybatisSqlInterceptor.class);
 
+    @Value("${mybatis.interceptor.enabled:true}")
+    private boolean interceptorEnabled;
+
     /**
      * mybatis的执行模块
      *
@@ -49,6 +53,9 @@ public class MybatisSqlInterceptor implements Interceptor {
      */
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
+        if (!interceptorEnabled) {
+            return invocation.proceed();
+        }
         JSONObject map = new JSONObject();
         String msg = "";
         String type = "";
