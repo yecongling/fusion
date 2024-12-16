918a35d398b871b2fb4b5f6d78fd2a1e36e17b63
ycl <499475142@qq.com>
2024-12-16 11:34:12 +0800

    fix(连接信息配置): 连接安全性
    数据库连接信息、redis连接信息进行加密处理，确保连接信息的安全性

--- fusion-framework/src/main/java/cn/net/fusion/framework/config/RedisConfig.java
+++ fusion-framework/src/main/java/cn/net/fusion/framework/config/RedisConfig.java
@@ -1,6 +1,9 @@
 package cn.net.fusion.framework.config;
 
+import cn.net.fusion.framework.security.EncryptionUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
@@ -15,6 +18,50 @@ import org.springframework.stereotype.Component;
  **/
 @Component
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.password}")
+    private String encryptedPassword;
+
+    @Value("${spring.data.redis.port}")
+    private Integer port;
+
+    @Value("${spring.data.redis.database}")
+    private Integer database;
+
+    /*  连接池相关信息  */
+
+    @Value("${spring.data.redis.lettuce.pool.max-idle}")
+    private Integer maxIdle;
+
+    @Value("${spring.data.redis.lettuce.pool.max-active}")
+    private Integer maxActive;
+
+    @Value("${spring.data.redis.lettuce.pool.max-idle}")
+    private Integer minIdle;
+
+    // 从环境变量获取秘钥
+    private static final String SECRET_KEY = System.getenv("FUSION_SECRET_KEY");
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() throws Exception {
+        // 解密密码
+        String password = EncryptionUtils.decrypt(encryptedPassword, SECRET_KEY);
+
+        // 配置redis连接工厂
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
+
+        configuration.setPassword(password);
+        configuration.setDatabase(database);
+
+        // 配置连接池
+        LettuceConnectionFactory connectionFactory = new LettuceConnectionFactory(configuration);
+        connectionFactory.setValidateConnection(true);
+        return connectionFactory;
+    }
+
     @Bean(name = "redisTemplate")
     public RedisTemplate<String, Object> getRedisTemplate(LettuceConnectionFactory factory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
