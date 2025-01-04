dfa0fbb2bc62c5f1743fb5d3ca5921e7f43834e6
ycl <499475142@qq.com>
2025-01-04 16:26:19 +0800

    feat(SQL脚本): 系统角色相关脚本
    修改系统角色相关的SQL脚本

--- database/tables/t_sys_role_menu.sql
+++ database/tables/t_sys_role_menu.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80040 (8.0.40-0ubuntu0.24.04.1)
  File Encoding         : 65001
 
- Date: 04/01/2025 09:53:19
+ Date: 04/01/2025 16:23:05
 */
 
 SET NAMES utf8mb4;
@@ -23,10 +23,14 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `t_sys_role_menu`;
 CREATE TABLE `t_sys_role_menu`
 (
-    `id`         bigint unsigned                                              NOT NULL COMMENT '主键ID',
-    `role_id`    bigint unsigned                                              NOT NULL COMMENT '角色ID',
-    `menu_id`    bigint unsigned                                              NOT NULL COMMENT '菜单id',
-    `operate_ip` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '操作IP',
+    `id`          bigint unsigned                                              NOT NULL COMMENT '主键ID',
+    `role_id`     bigint unsigned                                              NOT NULL COMMENT '角色ID',
+    `menu_id`     bigint unsigned                                              NOT NULL COMMENT '菜单id',
+    `operate_ip`  varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL COMMENT '操作IP',
+    `create_by`   bigint unsigned                                              NOT NULL COMMENT '创建人',
+    `create_time` datetime                                                     NOT NULL COMMENT '创建时间',
+    `update_by`   bigint unsigned                                              NOT NULL COMMENT '更新人',
+    `update_time` datetime                                                     NOT NULL COMMENT '更新时间',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
