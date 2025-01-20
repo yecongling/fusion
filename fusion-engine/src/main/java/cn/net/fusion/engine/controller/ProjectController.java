8813eb89812b00e8047ab08fd9ea20aab0e0d769
ycl <499475142@qq.com>
2025-01-20 13:52:32 +0800

    feat(项目管理): 项目管理
    项目管理controller添加

---
+++ fusion-engine/src/main/java/cn/net/fusion/engine/controller/ProjectController.java
@@ -0,0 +1,17 @@
+package cn.net.fusion.engine.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName ProjectController
+ * @Description 项目相关入口
+ * @Author ycl
+ * @Date 2025/1/20 10:25
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/engine/project")
+public class ProjectController {
+
+}
