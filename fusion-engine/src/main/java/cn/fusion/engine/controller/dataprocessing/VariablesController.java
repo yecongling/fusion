d2d764e28d76cdd0491cf8457fc8b028dbd47e62
ycl <499475142@qq.com>
2025-02-22 14:46:08 +0800

    feat(文件夹占领): 功能入口列举

---
+++ fusion-engine/src/main/java/cn/fusion/engine/controller/dataprocessing/VariablesController.java
@@ -0,0 +1,16 @@
+package cn.fusion.engine.controller.dataprocessing;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName VariablesController
+ * @Description 变量配置
+ * @Author 49947
+ * @Date 2025/2/22 14:23
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/engine/variables")
+public class VariablesController {
+}
---
+++ fusion-engine/src/main/java/cn/fusion/engine/controller/statistics/MessageSearchController.java
@@ -0,0 +1,16 @@
+package cn.fusion.engine.controller.statistics;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName MessageSearchController
+ * @Description 消息检索
+ * @Author 49947
+ * @Date 2025/2/22 14:44
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/engine/statistics/messageSearch")
+public class MessageSearchController {
+}
