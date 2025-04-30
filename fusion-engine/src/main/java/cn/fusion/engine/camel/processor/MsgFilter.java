1fc536ea8f55c9d116a5fc5d598331c516d421a7
ycl <499475142@qq.com>
2025-04-30 18:25:50 +0800

    feat(engine): 消息过滤器

--- fusion-engine/src/main/java/cn/fusion/engine/camel/processor/MsgFilter.java
+++ fusion-engine/src/main/java/cn/fusion/engine/camel/processor/MsgFilter.java
@@ -1,24 +1,39 @@
 package cn.fusion.engine.camel.processor;
 
+import cn.fusion.engine.camel.core.IActionExecutor;
+import cn.fusion.engine.camel.model.FlowNode;
 import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
+import org.springframework.stereotype.Component;
 
 /**
  * @ClassName MsgFilter
  * @Description 消息过滤器
- * @Author 49947
+ * @Author ycl
  * @Date 2025/2/8 16:52
  * @Version 1.0
  */
-public class MsgFilter implements Processor {
+@Component
+public class MsgFilter implements IActionExecutor {
 
     /**
-     * 消息处理
-     * @param exchange 消息交换对象
-     * @throws Exception 异常
+     * 判断执行器是否支持该操作
+     *
+     * @param actionType 操作类型
+     * @return true:支持，false:不支持
      */
     @Override
-    public void process(Exchange exchange) throws Exception {
+    public boolean support(String actionType) {
+        return false;
+    }
+
+    /**
+     * 执行操作
+     *
+     * @param node     节点
+     * @param exchange 消息交换对象
+     */
+    @Override
+    public void execute(FlowNode node, Exchange exchange) {
 
     }
 }
