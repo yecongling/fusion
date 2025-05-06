f74482fc083f9af61ca4174054865b169cb3828c
ycl <499475142@qq.com>
2025-05-06 17:57:32 +0800

    refactor(lombok): 操作员对象使用lombok

--- fusion-framework/src/main/java/cn/fusion/framework/core/SysOpr.java
+++ fusion-framework/src/main/java/cn/fusion/framework/core/SysOpr.java
@@ -3,6 +3,8 @@ package cn.fusion.framework.core;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.mybatisflex.annotation.Column;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
@@ -13,6 +15,8 @@ import java.util.List;
  * @Date 2024/11/4 20:43
  * @Version 1.0
  **/
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class SysOpr extends BaseEntity{
 
     /**
@@ -52,52 +56,4 @@ public class SysOpr extends BaseEntity{
     @Column(ignore = true)
     @JsonSerialize(using = ToStringSerializer.class)
     private List<Long> roleIds;
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getHomePath() {
-        return homePath;
-    }
-
-    public void setHomePath(String homePath) {
-        this.homePath = homePath;
-    }
-
-    public String getLoginIp() {
-        return loginIp;
-    }
-
-    public void setLoginIp(String loginIp) {
-        this.loginIp = loginIp;
-    }
-
-    public String getCurrentRoleId() {
-        return currentRoleId;
-    }
-
-    public void setCurrentRoleId(String currentRoleId) {
-        this.currentRoleId = currentRoleId;
-    }
-
-    public List<Long> getRoleIds() {
-        return roleIds;
-    }
-
-    public void setRoleIds(List<Long> roleIds) {
-        this.roleIds = roleIds;
-    }
 }
